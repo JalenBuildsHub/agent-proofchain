47c8cff8655fa6a45babdcc3c17709bd8ee581aa
JalenBuildsHub <contact@jalenbuilds.com>
2026-07-28 00:35:37 -0700

    feat: export runtime adapter contracts

--- src/proofchain/__init__.py
+++ src/proofchain/__init__.py
@@ -1,16 +1,35 @@
 """Public API for Agent ProofChain."""
 
+from .adapters import (
+    AdapterContractError,
+    AnthropicAdapter,
+    AuthenticatedRuntimeContext,
+    GoogleAdapter,
+    LocalRuntimeAdapter,
+    MappingRuntimeAdapter,
+    NormalizedRuntimeRequest,
+    OpenAIAdapter,
+    RuntimeAdapter,
+)
 from .admission import AdmissionDecision, AdmissionRequest, evaluate
 from .ledger import ReceiptLedger
 from .policy import AdmissionPolicy
 
 __all__ = [
+    "AdapterContractError",
     "AdmissionDecision",
     "AdmissionPolicy",
     "AdmissionRequest",
+    "AnthropicAdapter",
+    "AuthenticatedRuntimeContext",
+    "GoogleAdapter",
+    "LocalRuntimeAdapter",
+    "MappingRuntimeAdapter",
+    "NormalizedRuntimeRequest",
+    "OpenAIAdapter",
     "ReceiptLedger",
+    "RuntimeAdapter",
     "evaluate",
 ]
 
 __version__ = "0.1.0"
-
